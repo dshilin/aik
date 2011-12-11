2aa5c11a4041e1ee006154a983cb600ec186eb35
Dmitry Shilin <snayp@ubuntu-ror.(none)>
2011-12-11 22:23:13 +0400

    add animated menu

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+jQuery(document).ready(function(){
+
+jQuery("#menu td div").mouseenter(
+  function () {
+	jQuery(this).animate({
+		backgroundColor:"#be0000",
+    }, 500 );
+});
+
+jQuery("#menu td div").mouseleave(function() {
+	jQuery(this).animate({
+		backgroundColor:"#fff",
+    }, 50 );
+});
+
+});
